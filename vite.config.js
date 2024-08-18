6fc748812c9ecf863406007d95d4d5012f1a0e98
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-08-18 12:24:44 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,10 @@ export default defineConfig({
   base: "",
   plugins: [react()],
 })
+
+
+// {
+//   "version": 2,
+//   "builds": [{ "src": "server.js", "use": "@vercel/node" }],
+//   "routes": [{ "src": "/(.*)", "dest": "server.js" }]
+// }
